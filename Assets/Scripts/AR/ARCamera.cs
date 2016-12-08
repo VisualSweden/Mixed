756059e3bd7015542382db2e62c07fe5e5eab424
Jesper Tingvall <jesper.tingvall@hiq.se>
2016-12-08 17:05:32 +0100

    Add GUI

---
+++ Assets/Scripts/AR/ARCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ARCamera : MonoBehaviour {
+	void Start () {
+        ScriptEventSystem.Instance.OnSetARMode += OnSetARMode;
+        gameObject.SetActive(false);
+	}
+
+    private void OnSetARMode(bool b) {
+        gameObject.SetActive(b);
+    }
+}
---
+++ Assets/Scripts/ScriptEventSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptEventSystem : MonoBehaviour {
+    public delegate void voidDeletage();
+    public delegate void booldDeletage(bool b);
+
+    public event booldDeletage OnSetARMode;
+
+    public static ScriptEventSystem Instance;
+
+    void Awake() {
+        Instance = this;
+    }
+
+    public void SetARMode(bool arModeOn) {
+        if (OnSetARMode != null)
+            OnSetARMode(arModeOn);
+    }
+}
