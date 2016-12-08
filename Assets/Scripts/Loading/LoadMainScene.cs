f94452d6da6274b181e5337093e795ef9445b1ad
Jesper Tingvall <jesper.tingvall@hiq.se>
2016-12-08 14:54:58 +0100

    Add AR project with seperate scenes.

---
+++ Assets/Scripts/Loading/LoadMainScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadMainScene : MonoBehaviour {
+	void Start () {
+        Scene mainScene = SceneManager.GetSceneByName("MainScene");
+        if (mainScene == null || !mainScene.isLoaded)
+            SceneManager.LoadScene("MainScene");
+	}
+}
---
+++ Assets/Scripts/Loading/LoadScenes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScenes : MonoBehaviour {
+    public string[] Scenes;
+
+    private static bool doOnce;
+
+    void Awake() {
+        if (!doOnce) {
+            doOnce = true;
+            foreach (var scene in Scenes) {
+                SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+            }
+        }
+    }
+}
