c15e9b3e6c06310462fd3a4574f8c5300b974a94
Jesper Tingvall <jesper.tingvall@hiq.se>
2016-12-19 15:53:06 +0100

    Can rotate camera using gyroscope

---
+++ Assets/GyroscopeCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroscopeCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+		Input.location.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Input.gyro.attitude;
+		transform.Rotate (9f, 0f, 180f, Space.Self);
+		transform.Rotate (90f, 180f, 0f, Space.World);
+	}
+}
