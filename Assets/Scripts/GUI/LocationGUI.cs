2d59bee2c7026e8789fe32e937829b2398d3f6ca
Jesper Tingvall <jesper.tingvall@hiq.se>
2016-12-20 15:57:41 +0100

    Add GUI to Coffice AR location.

---
+++ Assets/Scripts/GUI/LocationGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocationGUI : MonoBehaviour {
+
+    public AddMapLocation location;
+
+	void Start () {
+        ScriptEventSystem.Instance.OnGoToLocation += Instance_OnGoToLocation;
+        ScriptEventSystem.Instance.OnSetMode += Instance_OnSetMode;
+        gameObject.SetActive(false);
+    }
+
+    private void Instance_OnSetMode(ScriptEventSystem.Mode m) {
+        if (m != ScriptEventSystem.Mode.AR) {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Instance_OnGoToLocation(Location l) {
+        if (l == location.Location)
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
+    }
+}
