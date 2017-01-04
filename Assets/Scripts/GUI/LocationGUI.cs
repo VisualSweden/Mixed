56587dd54576dccb0d237d669144336160a916fc
Jesper Tingvall <jesper.tingvall@hiq.se>
2017-01-04 11:51:54 +0100

    Hide GUI when video is playing.

--- Assets/Scripts/GUI/LocationGUI.cs
+++ Assets/Scripts/GUI/LocationGUI.cs
@@ -5,22 +5,31 @@ public class LocationGUI : MonoBehaviour {
 
     public AddMapLocation location;
 
-	void Start () {
+    private bool isOnLocation;
+    private bool isObjectVisible;
+    private bool arMode;
+
+    void Start() {
         ScriptEventSystem.Instance.OnGoToLocation += Instance_OnGoToLocation;
         ScriptEventSystem.Instance.OnSetMode += Instance_OnSetMode;
+
+        ScriptEventSystem.Instance.OnSoundObjectVisible += () => { isObjectVisible = true; UpdateVisible(); };
+        ScriptEventSystem.Instance.OnSoundObjectHidden += () => { isObjectVisible = false; UpdateVisible(); };
+
         gameObject.SetActive(false);
     }
 
     private void Instance_OnSetMode(ScriptEventSystem.Mode m) {
-        if (m != ScriptEventSystem.Mode.AR) {
-            gameObject.SetActive(false);
-        }
+        arMode = m == ScriptEventSystem.Mode.AR;
+        UpdateVisible();
+    }
+
+    private void UpdateVisible() {
+        gameObject.SetActive(isOnLocation && !isObjectVisible && arMode);
     }
 
     private void Instance_OnGoToLocation(Location l) {
-        if (l == location.Location)
-            gameObject.SetActive(true);
-        else
-            gameObject.SetActive(false);
+        isOnLocation = l == location.Location;
+        UpdateVisible();
     }
 }
