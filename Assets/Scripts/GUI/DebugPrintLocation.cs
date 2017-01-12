62720fca7cf0d7e2fe5b2e33d4c92a07784ebf25
Jesper Tingvall <jesper.tingvall@hiq.se>
2017-01-12 11:16:35 +0100

    Add debug text with current location.

---
+++ Assets/Scripts/GUI/DebugPrintLocation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DebugPrintLocation : MonoBehaviour {
+
+    private Text text;
+    private OnlineMapsLocationService location;
+
+    void Start() {
+        text = GetComponent<Text>();
+        location = MapManager.Instance.location;
+    }
+
+    void Update() {
+        text.text = "Lng " + location.position.x + "\nLat " + location.position.y;
+    }
+}
