039ee113fbb73a0e2a73dbc25ed506c8670c0abc
Jesper Tingvall <jesper.tingvall@hiq.se>
2017-01-16 10:32:50 +0100

    Tracks sound correctly.

--- Assets/Scripts/AR/ARTrackedVideo.cs
+++ Assets/Scripts/AR/ARTrackedVideo.cs
@@ -7,10 +7,15 @@ public class ARTrackedVideo : MonoBehaviour, Vuforia.ITrackableEventHandler {
     private TrackableBehaviour mTrackableBehaviour;
     private MediaPlayerCtrl mediaPlayer;
     private bool ignoreEnd = false;
+    private Camera mainCamera;
+
+    private bool isTracked;
+    private bool isVideoVisible;
 
     public float resetTime;
 
     void Awake() {
+        mainCamera = FindObjectOfType<VuforiaBehaviour>().GetComponentInChildren<Camera>();
         mediaPlayer = GetComponentInChildren<MediaPlayerCtrl>();
         mediaPlayer.OnEnd += delegate () {
             if(mediaPlayer.GetSeekPosition() > 0 && !ignoreEnd)
@@ -28,25 +33,53 @@ public class ARTrackedVideo : MonoBehaviour, Vuforia.ITrackableEventHandler {
         if (newStatus == TrackableBehaviour.Status.DETECTED ||
                newStatus == TrackableBehaviour.Status.TRACKED ||
                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
-            if (ScriptEventSystem.Instance.CurrentMode == ScriptEventSystem.Mode.AR)
-                OnTrackingFound();
+            if (ScriptEventSystem.Instance.CurrentMode == ScriptEventSystem.Mode.AR) {
+                //OnTrackingFound();
+                isTracked = true;
+            }
         } else {
             OnTrackingLost();
+            isTracked = false;
+            isVideoVisible = false;
+        }
+    }
+
+    void Update() {
+        if (isTracked) {
+            Vector3 cam = mainCamera.transform.forward;
+            Vector3 dir = mainCamera.transform.position - mediaPlayer.transform.position;
+            if (isVideoVisible) {
+                if (Vector3.Dot(cam, dir) > 0) {
+                    OnTrackingLost();
+                    isVideoVisible = false;
+                }
+
+            } else {
+                if (Vector3.Dot(cam, dir) < 0) {
+                    OnTrackingFound();
+                    isVideoVisible = true;
+                }
+
+            }
         }
     }
 
     private void OnSetMode(ScriptEventSystem.Mode m) {
-        if (m != ScriptEventSystem.Mode.AR)
+        if (m != ScriptEventSystem.Mode.AR) {
             OnTrackingLost();
+            mediaPlayer.SeekTo(0);
+        }
     }
 
     private void OnTrackingFound() {
+        isTracked = true;
         CancelInvoke("OnTrackingTimedOut");
         mediaPlayer.gameObject.SetActive(true);
         mediaPlayer.Play();
     }
 
     private void OnTrackingLost() {
+        isTracked = false;
         mediaPlayer.gameObject.SetActive(false);
         CancelInvoke("OnTrackingTimedOut");
         Invoke("OnTrackingTimedOut", resetTime);
@@ -60,7 +93,7 @@ public class ARTrackedVideo : MonoBehaviour, Vuforia.ITrackableEventHandler {
     private void OnTrackingTimedOut() {
         if (ScriptEventSystem.Instance.CurrentMode == ScriptEventSystem.Mode.AR) {
             mediaPlayer.SeekTo(0);
-            mediaPlayer.Play();
+            //mediaPlayer.Play();
         }
     }
 
