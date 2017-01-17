f9f94b2935f9b758ce5b68726024e65c92039830
Jesper Tingvall <jesper.tingvall@hiq.se>
2017-01-17 11:05:24 +0100

    Add bubbles to spot #90.

---
+++ Assets/Scripts/AR/Bubbles/Bubble.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bubble : MonoBehaviour {
+    public delegate void bubbleDelegate(Bubble bubble);
+
+    private Animator animator;
+
+    public Vector3 BaseVelocity;
+    public Vector3 Amplitude;
+    public float Frequency;
+    public float Lifetime;
+
+    public event bubbleDelegate OnBubbleDestroyed;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnEnable () {
+        Invoke("DestroyBubble", Lifetime);
+	}
+
+    public void DestroyBubble() {
+        animator.SetTrigger("Explode");
+    }
+
+    public void ExplodeAnimationFinished() {
+        gameObject.SetActive(false);
+        OnBubbleDestroyed(this);
+    }
+
+    void OnMouseUpAsButton() {
+        DestroyBubble();
+    }
+	
+	void Update () {
+        transform.localPosition += (BaseVelocity + Mathf.Sin(Frequency) * Amplitude) * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/AR/Bubbles/BubbleManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleManager : MonoBehaviour {
+
+    public int NumberOfBubbles;
+    public float SpawnRadius;
+
+    public GameObject BubblePrefab;
+
+    public float MinLifetime;
+    public float MaxLifetime;
+
+    public Vector3 BaseSpeed;
+    public Vector3 Amplitude;
+    public float MaxFrequency;
+
+    private Bubble[] Bubbles;
+
+    IEnumerator Start () {
+        Bubbles = new Bubble[NumberOfBubbles];
+        for(int i = 0; i<NumberOfBubbles; i++) {
+            GameObject Object = Instantiate<GameObject>(BubblePrefab);
+            Object.transform.SetParent(transform);
+            Bubbles[i] = Object.GetComponent<Bubble>();
+            Bubbles[i].OnBubbleDestroyed += delegate(Bubble b) { SetupBubble(b); };
+            SetupBubble(Bubbles[i]);
+            yield return new WaitForSeconds(Random.Range(MinLifetime, MaxLifetime)/NumberOfBubbles);
+        }
+	}
+
+    private void SetupBubble(Bubble bubble) {
+        bubble.transform.position = transform.position + Random.insideUnitSphere * SpawnRadius;
+        bubble.Lifetime = Random.Range(MinLifetime, MaxLifetime);
+        bubble.BaseVelocity = BaseSpeed;
+        bubble.Amplitude = Random.value * Amplitude;
+        bubble.Frequency = MaxFrequency * Random.value;
+        bubble.gameObject.SetActive(true);
+    }
+}
