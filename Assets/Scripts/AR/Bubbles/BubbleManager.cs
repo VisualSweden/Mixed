66cba0441a3c8953fb549aa5682d930ba045c7a9
Jesper Tingvall <jesper.tingvall@hiq.se>
2017-01-18 13:22:40 +0100

    Bubbles does not respawn.

--- Assets/Scripts/AR/Bubbles/BubbleManager.cs
+++ Assets/Scripts/AR/Bubbles/BubbleManager.cs
@@ -14,20 +14,35 @@ public class BubbleManager : MonoBehaviour {
     public Vector3 BaseSpeed;
     public Vector3 Amplitude;
     public float MaxFrequency;
+    public float SpawnDelay;
 
     private Bubble[] Bubbles;
 
-    IEnumerator Start () {
+    void Awake () {
         Bubbles = new Bubble[NumberOfBubbles];
         for(int i = 0; i<NumberOfBubbles; i++) {
             GameObject Object = Instantiate<GameObject>(BubblePrefab);
             Object.transform.SetParent(transform);
             Bubbles[i] = Object.GetComponent<Bubble>();
-            Bubbles[i].OnBubbleDestroyed += delegate(Bubble b) { SetupBubble(b); };
-            SetupBubble(Bubbles[i]);
-            yield return new WaitForSeconds(Random.Range(MinLifetime, MaxLifetime)/NumberOfBubbles);
+           // Bubbles[i].OnBubbleDestroyed += delegate(Bubble b) { SetupBubble(b); };
         }
 	}
+
+    void OnEnable() {
+        StopCoroutine(AnimateBubbles());
+        StartCoroutine(AnimateBubbles());
+    }
+
+    private IEnumerator AnimateBubbles() {
+        for (int i = 0; i < NumberOfBubbles; i++) {
+            Bubbles[i].gameObject.SetActive(false);
+        }
+
+        for(int i = 0; i<NumberOfBubbles; i++) {
+            SetupBubble(Bubbles[i]);
+            yield return new WaitForSeconds(SpawnDelay);
+        }
+    }
 
     private void SetupBubble(Bubble bubble) {
         bubble.transform.position = transform.position + Random.insideUnitSphere * SpawnRadius;
