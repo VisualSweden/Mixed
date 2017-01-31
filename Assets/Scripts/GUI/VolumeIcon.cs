0113008934d74d447b4571b8eec76bfa36dd0467
Jesper Tingvall <jesper.tingvall@hiq.se>
2017-01-31 16:28:31 +0100

    Add text telling users to wear headphones instead of icon.

--- Assets/Scripts/GUI/VolumeIcon.cs
+++ Assets/Scripts/GUI/VolumeIcon.cs
@@ -3,16 +3,10 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class VolumeIcon : MonoBehaviour {
-	private Toggle toggle;
 
 	void Start () {
 		ScriptEventSystem.Instance.OnSoundObjectVisible += () => {gameObject.SetActive(true);};
 		ScriptEventSystem.Instance.OnSoundObjectHidden += () => {gameObject.SetActive(false);};
-
-		toggle = GetComponent<Toggle> ();
-		toggle.onValueChanged.AddListener (delegate (bool b){
-			ScriptEventSystem.Instance.SetSoundOn (b);
-		});
 		gameObject.SetActive (false);
 	}
 }
