dd089fb70bbd1986582f08c14bdc70f0819bd8ef
Jesper Tingvall <jesper.tingvall@hiq.se>
2017-02-08 11:53:26 +0100

    Solved null error on start with GPS arrows.

--- Assets/Prefabs/AR/ArrowPointing.cs
+++ Assets/Prefabs/AR/ArrowPointing.cs
@@ -2,23 +2,18 @@
 using System.Collections;
 
 public class ArrowPointing : MonoBehaviour {
-
     public Camera PositionCamera;
-
     public GameObject pointTowards;
-
     public SpriteRenderer sprite;
-
     public AnimationCurve AlphaCurve;
 
-    // Use this for initialization
     void Start () {
-        //sprite.transform.localPosition = new Vector3(0, Random.Range(-1f, 0f), 0);
-        PositionCamera = FindObjectOfType<GyroscopeCamera>().GetComponent<Camera>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        GyroscopeCamera camera = FindObjectOfType<GyroscopeCamera>();
+        if (camera)
+            PositionCamera = camera.GetComponent<Camera>();
+    }
+
+    void Update () {
         transform.position = PositionCamera.transform.position + PositionCamera.transform.forward * 6;
         transform.LookAt(pointTowards.transform.position, (transform.position - PositionCamera.transform.position).normalized);
 
@@ -31,6 +26,5 @@ public class ArrowPointing : MonoBehaviour {
         Color C = sprite.color;
         C.a = AlphaCurve.Evaluate(s);
         sprite.color = C;
-
 	}
 }
